dcfc94bbb4c89978707dd295c84f854559292ce7
Chichi Zhang <93120868+13011036799@users.noreply.github.com>
2023-05-12 22:10:42 +0800

    Update section3_Console Programming.py

--- section3_Console Programming.py
+++ section3_Console Programming.py
@@ -6,23 +6,20 @@ and prints the equivalent weight on Mars.
 
 def main():
     # Fill this function out!
-    earth_w = int(input("Enter a weight on Earth: ") )
+    earth_w = float(input("Enter a weight on Earth: ") )
     mars_w = 37.8 / 100 * earth_w
-    print(f"The equivalent weight on Mars: {round(mars_w,2)}")
+    # print(f"The equivalent weight on Mars: {round(mars_w,2)}")
+    # print("The equivalent weight on Mars: ",round(mars_w,2)) # easiest
+    # print("The equivalent weight on Mars: "+str(round(mars_w,2)))
+    
+    print(f"The equivalent weight on Mars: {mars_w:.2f}")
+    
+    # print(f"string {variables}")
 
 if __name__ == "__main__":
     main()
     
 # Planetary Weight Calculator
-'''
-Mercury: 37.6%
-Venus: 88.9%
-Mars: 37.8%
-Jupiter: 236.0%
-Saturn: 108.1%
-Uranus: 81.5%
-Neptune: 114.0%
-'''
 """
 Prompts the user for a weight on Earth
 and a planet (in separate inputs). Then 
@@ -33,35 +30,36 @@ the first letter as uppercase, and you do not need
 to handle the case where a user types in something 
 other than one of the planets (that is not Earth). 
 """
+FRACTION = {"Mercury":37.6 / 100,"Venus":88.9 / 100,"Mars":37.8 / 100,"Jupiter": 236.0 / 100,"Saturn":108.1 / 100,"Uranus":81.5 / 100,"Neptune":114.0 / 100}
 
 def main():
     # Fill this function out!
-    earth_w = int(input("Enter a weight on Earth: "))
-    # name_planet = capitalize(input("Enter a planet: "))
-    name_planet = input(("Enter a planet: "))
+    earth_w = float(input("Enter a weight on Earth: "))
     
     loop = True
     while loop:
+        name_planet = input("Enter a planet: ")
+        name_planet = name_planet.capitalize()
         if name_planet == "Mercury":
-            planet_w = 37.6 / 100 * earth_w
+            planet_w = FRACTION["Mercury"] * earth_w
             loop = False
         elif name_planet == "Venus":
-            planet_w = 88.9 / 100 * earth_w
+            planet_w = FRACTION["Venus"] * earth_w
             loop = False
         elif name_planet == "Mars":
-            planet_w = 37.8 / 100 * earth_w
+            planet_w = FRACTION["Mars"] * earth_w
             loop = False
         elif name_planet == "Jupiter":
-            planet_w = 236.0 / 100 * earth_w
+            planet_w = FRACTION["Jupiter"] * earth_w
             loop = False
         elif name_planet == "Saturn":
-            planet_w = 108.1 / 100 * earth_w
+            planet_w = FRACTION["Saturn"] * earth_w
             loop = False
         elif name_planet == "Uranus":
-            planet_w = 81.5 / 100 * earth_w
+            planet_w = FRACTION["Uranus"] * earth_w
             loop = False
         elif name_planet == "Neptune":
-            planet_w = 114.0 / 100 * earth_w
+            planet_w = FRACTION["Neptune"] * earth_w
             loop = False
         else :
             print("Your input is invalid. Please try again.")
@@ -70,4 +68,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
