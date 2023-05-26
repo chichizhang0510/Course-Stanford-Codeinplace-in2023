0a4e3a01ad6d137d1c5a95aacb670302b110d6c6
Chichi Zhang <93120868+13011036799@users.noreply.github.com>
2023-05-27 03:17:03 +0800

    Update section2_Karel_Problem_Solving.py

--- section2_Karel_Problem_Solving.py
+++ section2_Karel_Problem_Solving.py
@@ -62,6 +62,16 @@ def turn_around():
 
 # ------------------ leader's codes--------------------------
 
+# Section 2 Solutions
+# from karel.stanfordkarel import *
+
+# """
+# Each row starts in front of a stack of beepers. Karel should pick them
+# up, one at a time, and spread them down the row. 
+# Caution! Karel can't count, and starts with infinite beepers infinite
+# her bag. How can you solve this puzzle?
+# """
+
 # def main():
 #     move()
 #     spread()
@@ -98,6 +108,7 @@ def turn_around():
 #     turn_around()
 #     move()
 #     turn_around()
+
 
 # There is no need to edit code beyond this point
 if __name__ == '__main__':
