1bff50c2a3e7c3d390f107312de8b742868a47f3
Chichi Zhang <93120868+13011036799@users.noreply.github.com>
2023-06-05 20:02:18 +0800

    Create code12_DrawFlag.py

---
+++ code12_DrawFlag.py
@@ -0,0 +1,15 @@
+'''find details of the instruction from the readme file'''
+
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 450
+CANVAS_HEIGHT = 300
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    # TODO, your code here
+    canvas.create_rectangle(0,0,CANVAS_WIDTH, CANVAS_HEIGHT/2,"red")
+
+if __name__ == '__main__':
+    main()
